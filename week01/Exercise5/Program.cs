17440931aaa1235c4fa32e5e110198c789d52d78
German Sanchez Lobo <germanslobo@gmail.com>
2026-06-30 21:47:51 -0400

    Update Exercise 5 Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        
+        DisplayGreeting(userName);
+        
+        int squaredNumber = SquareNumber(userNumber);
+        int cubedNumber = CubeNumber(userNumber);
+        
+        DisplayResult(userName, squaredNumber, cubedNumber);
+    }
+    
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    
+    static void DisplayGreeting(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+    
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    
+    static int CubeNumber(int number)
+    {
+        return number * number * number;
+    }
+    
+    static void DisplayResult(string name, int squared, int cubed)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+        Console.WriteLine($"{name}, the cube of your number is {cubed}");
     }
 }
