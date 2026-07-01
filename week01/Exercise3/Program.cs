834a11a7bcee14f1f3dd4037cded6889c8f183ad
German Sanchez Lobo <germanslobo@gmail.com>
2026-06-30 21:34:35 -0400

    Update Exercise 3 Guess Magic Number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Ask user for the magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+        int guess = 0; // Initialize to a value that won't match
+        
+        // Keep looping until the guess matches the magic number
+        while (guess != magicNumber)
+        {
+            // Ask user for their guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            // Determine if they need to guess higher, lower, or got it
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
