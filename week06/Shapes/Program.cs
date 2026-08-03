95f7a38c4eaddf10c89bba6b10109fb4f9e4b7d2
German Sanchez Lobo <germanslobo@gmail.com>
2026-08-03 08:58:10 -0400

    Complete W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+
+namespace Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // 1. Create a list that holds Shape objects
+        List<Shape> shapes = new List<Shape>();
+
+        // 2. Add different instances to the list
+        shapes.Add(new Square("Red", 5.0));
+        shapes.Add(new Rectangle("Blue", 4.0, 6.0));
+        shapes.Add(new Circle("Green", 3.0));
+
+        // 3. Iterate through the list and display color and area polymorphic behavior
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
